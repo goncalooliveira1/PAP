24b2fdab9ab087a64512906806b8cee04da9fc45
Gonçalo Maria Oliveira <2025a103798@aerp.pt>
2025-04-22 14:33:36 +0100

    Sumário
    dsdas

--- airtraveller/lib/pages/mainpage.dart
+++ airtraveller/lib/pages/mainpage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../commentspage.dart';
+import 'commentspage.dart';
 import 'profile.dart';
 import 'settings.dart';
 
